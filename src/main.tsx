56134ad79567302f20f7937b3e395371b0f6f3db
dewanshDT <dewansh.dt@gmail.com>
2023-12-10 03:26:40 +0530

    relocated router

--- src/main.tsx
+++ src/main.tsx
@@ -2,20 +2,10 @@ import React from "react"
 
 import ReactDOM from "react-dom/client"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
-import { RouterProvider, createBrowserRouter } from "react-router-dom"
-
-import { RootList } from "./routes"
-import { Layout } from "./components"
+import { RouterProvider } from "react-router-dom"
 
 import "./index.css"
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    children: [{ index: true, element: <RootList /> }],
-  },
-])
+import router from "./routes"
 
 const queryClient = new QueryClient({
   defaultOptions: { queries: { refetchOnWindowFocus: false } },
---
+++ src/routes/index.tsx
@@ -0,0 +1,16 @@
+import { Navigate, createBrowserRouter } from "react-router-dom"
+import { Layout } from "../components"
+import ListView from "./ListView"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { index: true, element: <Navigate to="/issues/active" /> },
+      { path: "issues/:status", element: <ListView /> },
+    ],
+  },
+])
+
+export default router
