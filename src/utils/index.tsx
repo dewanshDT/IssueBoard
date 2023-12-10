15e2d6376da15f4c4d271d7dded53bcdcee9cd5d
dewanshDT <dewansh.dt@gmail.com>
2023-12-10 05:55:58 +0530

    moved color checker to utils

--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -1,4 +1,4 @@
-import { ticket } from "../types"
+import { label, ticket } from "../types"
 
 export const fetchIssues = async ({ pageParam = 1 }) => {
   console.log(pageParam)
@@ -10,4 +10,29 @@ export const fetchIssues = async ({ pageParam = 1 }) => {
   } = await res.json()
 
   return data
+}
+
+export const getLabelColor = (label: label) => {
+  switch (label) {
+    case "Bug":
+      return "bg-red-500"
+    case "Feature":
+      return "bg-blue-500"
+    case "Performance":
+      return "bg-yellow-500"
+    case "Security":
+      return "bg-purple-500"
+    case "Documentation":
+      return "bg-green-500"
+    case "User Request":
+      return "bg-pink-500"
+    case "Immediate":
+      return "bg-orange-500"
+    case "Next Release":
+      return "bg-indigo-500"
+    case "Major Release":
+      return "bg-gray-500"
+    default:
+      return "bg-gray-500"
+  }
 }
