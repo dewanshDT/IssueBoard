998a15c8ac7bfbb41058828e85a56848edd4f638
dewanshDT <dewansh.dt@gmail.com>
2023-12-12 04:53:49 +0530

    doc link

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,7 +1,19 @@
+import { LuGithub } from "react-icons/lu"
+import { Link } from "react-router-dom"
+
 const Header = () => {
   return (
-    <header className="px-6 py-3 border-b border-neutral-800">
-      <h1 className="uppercase font-semibold text-sm">hoppscotch</h1>
+    <header className="px-6 py-2 border-b border-neutral-800 flex justify-between items-center">
+      <div>
+        <h1 className="uppercase font-semibold text-sm">hoppscotch</h1>
+      </div>
+      <div className="">
+        <Link to="https://github.com/dewanshDT/IssueBoard" target="_blank">
+          <div className="hover:bg-neutral-950 p-1.5 text-base rounded-md">
+            <LuGithub />
+          </div>
+        </Link>
+      </div>
     </header>
   )
 }
