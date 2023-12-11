66476cc447766228750008ef47cfaec60d14b3a5
dewanshDT <dewansh.dt@gmail.com>
2023-12-12 03:47:53 +0530

    status bar number formatting and status text

--- src/components/Layout/StatusBar.tsx
+++ src/components/Layout/StatusBar.tsx
@@ -1,9 +1,4 @@
 import React from "react"
-import { ticket } from "../../types"
-import {
-  FetchNextPageOptions,
-  InfiniteQueryObserverResult,
-} from "@tanstack/react-query"
 import { twMerge } from "tailwind-merge"
 
 interface Props extends React.HTMLAttributes<HTMLDivElement> {
@@ -11,16 +6,12 @@ interface Props extends React.HTMLAttributes<HTMLDivElement> {
   isFetching: boolean
   hasNextPage: boolean
   isFetchingNextPage: boolean
-  fetchNextPage: (
-    options?: FetchNextPageOptions | undefined
-  ) => Promise<InfiniteQueryObserverResult<ticket[], Error>>
 }
 
 const StatusBar: React.FC<Props> = ({
   itemsLoaded,
   isFetching,
   isFetchingNextPage,
-  fetchNextPage,
   hasNextPage,
   ...props
 }) => {
@@ -29,29 +20,29 @@ const StatusBar: React.FC<Props> = ({
       {...props}
       className={twMerge(
         props.className,
-        "border-t border-neutral-800 p-2 text-xs text-neutral-400 flex justify-between"
+        "border-t border-neutral-700 p-2 text-xs text-neutral-300 flex justify-between"
       )}
     >
       <div>
         {isFetching && !isFetchingNextPage ? (
-          <span className="text-green-400">Fetching...</span>
+          <span className="text-green-400 cursor-default">Fetching...</span>
         ) : (
-          <button
-            onClick={() => fetchNextPage()}
-            disabled={!hasNextPage || isFetchingNextPage}
-          >
+          <div>
             {isFetchingNextPage ? (
               <span className="text-yellow-400">Loading more...</span>
             ) : hasNextPage ? (
-              "Load More"
+              "Scroll to Load more"
             ) : (
               "Nothing more to load :("
             )}
-          </button>
+          </div>
         )}
       </div>
       <div className="">
-        <div className="">items loaded: {itemsLoaded}</div>
+        <div className="">
+          Items loaded:{" "}
+          {itemsLoaded?.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+        </div>
       </div>
     </div>
   )
