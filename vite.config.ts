2cb7847650c37cc27183fe84dd7088212e04e480
dewanshDT <dewansh.dt@gmail.com>
2023-12-12 05:41:25 +0530

    github pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,18 @@ import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react-swc"
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-  },
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    server: {
+      port: 3000,
+    },
+    base: "/",
+  }
+
+  if (command !== "serve") {
+    config.base = "/IssueBoard/"
+  }
+
+  return config
 })
