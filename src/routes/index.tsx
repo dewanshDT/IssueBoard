7e87ff7a4682149327df511d4b419dd5eddf2313
dewanshDT <dewansh.dt@gmail.com>
2023-12-12 05:44:57 +0530

    basename

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,18 +2,21 @@ import { Navigate, createBrowserRouter } from "react-router-dom"
 import { Layout } from "../components"
 import ListView from "./ListView"
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    children: [
-      { index: true, element: <Navigate to="/issues/active" /> },
-      {
-        path: "issues/:status",
-        element: <ListView />,
-      },
-    ],
-  },
-])
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        { index: true, element: <Navigate to="/issues/active" /> },
+        {
+          path: "issues/:status",
+          element: <ListView />,
+        },
+      ],
+    },
+  ],
+  { basename: import.meta.env.DEV ? "/" : "/IssueBoard/" }
+)
 
 export default router
